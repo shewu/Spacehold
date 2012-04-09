119982a286c79a82e1205eca2e5b20c5d8a228f9
Victor Youk <razorracing@yahoo.com>
2012-04-09 06:37:16 -0400

    Added project files

---
+++ spaceholdFinal/src/com/a/space/MitersActivity.java
@@ -0,0 +1,14 @@
+package com.a.space;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MitersActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.miters);
+		
+	}
+}
