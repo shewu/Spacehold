c48a24bfee28692a31bc3da0edaceca4eb128d7b
shewu <shewu19@gmail.com>
2012-04-24 18:45:14 -0400

    added a new welcome view for new users

---
+++ spaceholdFinal/src/com/a/space/SetupWelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.a.space;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetupWelcomeActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setup_welcome_activity);
+	}
+}
