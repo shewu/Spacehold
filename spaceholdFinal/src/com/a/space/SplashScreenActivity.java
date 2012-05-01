a34abf094a6a42af8da23d8cab55924238ee6f3f
shewu <shewu19@gmail.com>
2012-05-01 03:15:16 -0400

    welcome screens do not need threads

--- spaceholdFinal/src/com/a/space/SplashScreenActivity.java
+++ spaceholdFinal/src/com/a/space/SplashScreenActivity.java
@@ -10,38 +10,26 @@ public class SplashScreenActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu);
+		
 		/** set time to splash out */
 		final int welcomeScreenDelay = 1000;
-		/** create a thread to show splash up to splash time */
-		Thread welcomeThread = new Thread() {
-			@Override
-			public void run() {
-				try {
-					super.run();
-					/**
-					 * use while to get the splash time. Use sleep() to increase
-					 * the wait variable for every 100L.
-					 */
-					sleep(welcomeScreenDelay);
-				} catch (Exception e) {
-					e.printStackTrace();
-				} finally {
-					/**
-					 * Called after splash times up. Do some action after splash
-					 * times up. Here we moved to another main activity class
-					 */
-					SharedPreferences settings = getSharedPreferences(SHUtil.PREFS_NAME, 0);
-					boolean hasRun = settings.getBoolean(SHUtil.HAS_RUN, false);
-					if (!hasRun) {
-						startActivity(new Intent(SplashScreenActivity.this, SetupWelcomeActivity.class));
-					} else {
-						startActivity(new Intent(SplashScreenActivity.this, MainScreenActivity.class));
-					}
-					finish();
-				}
+		try {
+			Thread.sleep(welcomeScreenDelay);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			/**
+			 * Called after splash times up. Do some action after splash
+			 * times up. Here we moved to another main activity class
+			 */
+			SharedPreferences settings = getSharedPreferences(SHUtil.PREFS_NAME, 0);
+			boolean hasRun = settings.getBoolean(SHUtil.HAS_RUN, false);
+			if (!hasRun) {
+				startActivity(new Intent(SplashScreenActivity.this, SetupWelcomeActivity.class));
+			} else {
+				startActivity(new Intent(SplashScreenActivity.this, MainScreenActivity.class));
 			}
-		};
-		welcomeThread.start();
-
+			finish();
+		}
 	}
 }
