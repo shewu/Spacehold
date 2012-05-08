4a29bd40cb3ec1170920fbf5d190be874b3963cb
Sherry Wu <shewu19@gmail.com>
2012-05-07 20:51:46 -0400

    testing

--- spacehold/fn.php
+++ spacehold/fn.php
@@ -12,11 +12,12 @@ $DB_LOGIN = "shewu";
 $SPACES_TBL = "spaces";
 $PEOPLE_TBL = "people";
 $PASSWD_FILE = "password.txt"
-/*
+
 function getPassword() {
     return file_get_contents($PASSWD_FILE);
 }
 
+/*
 function addSpace($spaceName) {
     $con = mysql_connect($DB_SERVER, $DB_LOGIN, getPassword());
     if (!mysql_select_db($DB_NAME)) {
