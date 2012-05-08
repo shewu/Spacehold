197a5b9f157789a22221f7e65bc9fa1f5326accd
Sherry Wu <shewu19@gmail.com>
2012-05-07 23:11:31 -0400

    testing

--- spacehold/fn.php
+++ spacehold/fn.php
@@ -23,7 +23,8 @@ function addSpace($spaceName) {
     global $DB_NAME, $DB_SERVER, $DB_LOGIN;
     global $SPACES_TBL, $PEOPLE_TBL;
 
-    $con = mysql_connect($DB_SERVER, $DB_LOGIN, getPassword());
+    //$con = mysql_connect($DB_SERVER, $DB_LOGIN, getPassword());
+    $con = mysql_connect('mysql.mit.edu', 'shewu', getPassword());
     if (!$con) {
         echo "Failed to connect to database.";
         die();
