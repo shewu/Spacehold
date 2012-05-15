5044d30ad2091197fd4ac047b1966db16cab9430
Sherry Wu <shewu19@gmail.com>
2012-05-15 14:19:27 -0400

    testing

--- spacehold/roster.php
+++ spacehold/roster.php
@@ -7,7 +7,7 @@ include 'fn.php';
 <select>
 <?
 $spaces = file_get_contents('http://shewu.scripts.mit.edu/Spacehold/spacehold/r.php?cmd=getspaces&tok=21W.789');
-$spaces = preg_split('/[\s,]+/', $spaces);
+$spaces = preg_split('/[\s,]+/', trim($spaces));
 print_r($spaces);
 foreach ($spaces as $space) {
     echo "<option value=$space>$space</option>\n";
