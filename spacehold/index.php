2c9054fbb76065eedaec7f8c85e61eba58b7b12c
Sherry Wu <shewu19@gmail.com>
2012-05-15 14:09:17 -0400

    added new roster.php to see who is there

--- spacehold/index.php
+++ spacehold/index.php
@@ -1,9 +1,16 @@
 <?
+if (!isset($_POST["submit"])) {
 include 'static/header.php';
 ?>
 <body>
+<form method=post action="<?php echo $_SERVER['PHP_SELF'];?>">
+<p>Sign in with your OpenID:</p>
+<input type=text name=id size=30 /> <input type=submit name=submit value="Log in" />
+</form>
 </body>
 <?
 include 'static/footer.php';
+} else {
+}
 ?>
 
---
+++ spacehold/roster.php
@@ -0,0 +1,21 @@
+<?
+include 'static/header.php';
+include 'fn.php';
+?>
+<body>
+<form method=post action="<?echo $_SERVER['PHP_SELF'];?>">
+<select>
+<?
+$spaces = file_get_contents('http://shewu.scripts.mit.edu/Spacehold/spacehold/r.php?cmd=getspaces&tok=21W.789');
+$spaces = explode('\n', $spaces);
+foreach ($spaces as $space) {
+    echo "<option value=$space>$space</option>\n";
+}
+?>
+</select>
+</form>
+</body>
+<?
+include 'static/footer.php';
+?>
+
