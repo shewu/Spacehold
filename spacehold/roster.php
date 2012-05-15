0f4196e8ad143744381d1efd7a5710f6d3d09328
Sherry Wu <shewu19@gmail.com>
2012-05-15 14:26:33 -0400

    need to finish roster.php

--- spacehold/roster.php
+++ spacehold/roster.php
@@ -4,17 +4,27 @@ include 'fn.php';
 ?>
 <body>
 <form method=post action="<?echo $_SERVER['PHP_SELF'];?>">
-<select>
+<select name='space'>
 <?
 $spaces = file_get_contents('http://shewu.scripts.mit.edu/Spacehold/spacehold/r.php?cmd=getspaces&tok=21W.789');
 $spaces = preg_split('/[\s,]+/', trim($spaces));
-print_r($spaces);
 foreach ($spaces as $space) {
     echo "<option value=$space>$space</option>\n";
 }
 ?>
 </select>
+<input type=submit value=Submit name=submit />
 </form>
+<?
+if (isset($_POST['submit'])) {
+    $leSpace = $_POST['space'];
+    $people = file_get_contents("http://shewu.scripts.mit.edu/Spacehold/spacehold/r.php?cmd=spaceinfo&space=$leSpace&tok=21W.789");
+    $people = preg_split('/[\s,]+/', trim($people));
+    foreach ($people as $person) {
+        // TODO
+    }
+}
+?>
 </body>
 <?
 include 'static/footer.php';
