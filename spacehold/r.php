c7b2eaa2378e85fc74255d873969a75491feba12
Sherry Wu <shewu19@gmail.com>
2012-05-15 17:53:36 -0400

    forgot to locally add auth token

--- spacehold/r.php
+++ spacehold/r.php
@@ -2,24 +2,29 @@
 
 include 'fn.php';
 
+$tok = $_GET["tok"];
 $cmd = $_GET["cmd"];
 
-if (strcmp($cmd, "getspaces") == 0) {
-    getSpaces();
-} else if (strcmp($cmd, "spaceinfo") == 0) {
-    $which = $_GET["space"];
-    getPeopleAtSpace($which);
-} else if (strcmp($cmd, "addspace") == 0) {
-    $which = $_GET["space"];
-    addSpace($which);
-} else if (strcmp($cmd, "addperson") == 0) {
-    $space = $_GET["space"];
-    $person = $_GET["person"];
-    addPersonToSpace($person, $space);
-} else if (strcmp($cmd, "removeperson") == 0) {
-    $space = $_GET["space"];
-    $person = $_GET["person"];
-    removePersonFromSpace($person, $space);
+if (strcmp($tok, "21W.789") == 0) {
+    if (strcmp($cmd, "getspaces") == 0) {
+        getSpaces();
+    } else if (strcmp($cmd, "spaceinfo") == 0) {
+        $which = $_GET["space"];
+        getPeopleAtSpace($which);
+    } else if (strcmp($cmd, "addspace") == 0) {
+        $which = $_GET["space"];
+        addSpace($which);
+    } else if (strcmp($cmd, "addperson") == 0) {
+        $space = $_GET["space"];
+        $person = $_GET["person"];
+        addPersonToSpace($person, $space);
+    } else if (strcmp($cmd, "removeperson") == 0) {
+        $space = $_GET["space"];
+        $person = $_GET["person"];
+        removePersonFromSpace($person, $space);
+    } else {
+        echo "-1\n";
+    }
 } else {
     echo "-1\n";
 }
